63c109f8d6157e6e46c14604bdc4f471cfdda8bd
Unknown <ondrusekjoshua@gmail.com>
2018-08-01 17:56:46 -0500

    Removed some commented code

--- app/quote.js
+++ app/quote.js
@@ -14,6 +14,7 @@ var Quote = module.exports = {
                     console.error(err.message);
                 }
                 console.log('Getting: ' + row.quote);
+                db.close();
                 callback(row.quote);
             });
         });
@@ -53,50 +54,3 @@ var Quote = module.exports = {
         });
     }
 }
-
-// var Quote = module.exports = {
-//     get_quote: function(callback) {
-//         var file = __dirname + '/tmp/quote.json';
-//         if (fs.existsSync(file)) {
-//             var obj = jsonfile.readFileSync(file);
-//             console.log('READ OBJECT: ' + obj.quote);
-//             if (obj.quote) {
-//                 callback(obj.quote);
-//             } else {
-//                 callback('NO QUOTE');
-//             }
-//         } else {
-//             callback('NO QUOTE');
-//         }
-//     },
-//     save_new_quote: function(callback) {
-//         let db = new sqlite3.Database('../quotes.db', sqlite3.OPEN_READWRITE, (err) => {
-//             if (err) {
-//                 return console.error(err.message);
-//             }
-
-//             console.log('Connected to the quotes database.');
-//             var sql = 'Select quote, id from quotes where chosen = 0 order by random() limit 1';
-
-//             db.get(sql, [], (err, row) => {
-//                 if (err) {
-//                     console.error(err.message);
-//                 }
-//                 var file = __dirname + '/tmp/quote.json';
-//                 var obj = {quote: row.quote};
-//                 if (!fs.existsSync(file)) {
-//                     fs.writeFileSync(file, '', function(err) {
-//                         if (err) {
-//                             console.error(err.message)
-//                         }
-//                         console.log('File created!');
-//                     });
-//                 }
-//                 jsonfile.writeFileSync(file, obj);
-//                 console.log('Saving: ' + row.quote);
-//                 console.log(row.id);
-//                 callback();
-//             });
-//         });
-//     }
-// }
